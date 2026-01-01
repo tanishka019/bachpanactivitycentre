7707a09323649d8b0b9af0ce11c67e718a20c7a5
tanishka019 <1903tanishka@gmail.com>
2026-01-01 20:49:19 +0530

    minor changes

--- src/sections/EnrollmentForm.jsx
+++ src/sections/EnrollmentForm.jsx
@@ -48,7 +48,7 @@ export default function EnrollmentForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await fetch("PASTE_NEW_SCRIPT_URL_HERE", {
+    await fetch("https://script.google.com/macros/s/AKfycbwhPWN21oyxWWuwGKbJrrDutrAPFb77qFYEyET8WUVGtMqLRfLQHSCz7VohcYeNx8PI/exec", {
       method: "POST",
       body: JSON.stringify(formData),
       mode: "no-cors",
