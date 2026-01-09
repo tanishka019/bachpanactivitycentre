d57ce373d3aee22d287d9fc668ce0ff9ff5be63e
tanishka019 <1903tanishka@gmail.com>
2026-01-09 20:18:09 +0530

    facilities changed

--- src/sections/Facilities.jsx
+++ src/sections/Facilities.jsx
@@ -1,6 +1,6 @@
 import styles from "./Facilities.module.css";
 
-const VideoSection = () => {
+const Facilities = () => {
   return (
     <section className={styles.videoSection}>
       <h2 className={styles.heading}>Message from Our Founder</h2>
